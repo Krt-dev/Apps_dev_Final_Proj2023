0b147eeede6183f44bfc4d8bae376d1f0ef1c752
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2023-12-14 01:42:51 +0800

    added more textfield and text area in the form and add button for submission

--- appsdevfinal-proj/src/app/add-products/page.tsx
+++ appsdevfinal-proj/src/app/add-products/page.tsx
@@ -9,6 +9,26 @@ export default function addProducts() {
                     name="namne"
                     placeholder="Name"
                     className="mb-3 w-full input input-bordered" />
+                    <textarea 
+                    required
+                    name="description"
+                    placeholder="Description"
+                    className="textarea-bordered textarea mb-3 w-full"
+                    />
+                    <input 
+                    required
+                    name="imageUrl"
+                    placeholder="Image Url"
+                    type="url"
+                    className="mb-3 w-full input input-bordered"
+                    />
+                    <input
+                    required
+                    name="price"
+                    placeholder="Price"
+                    type="number"
+                    className="mb-3 w-full input input-bordered" />
+                    <button type="submit" className="btn btnn-primary btn-block">Add Products</button>
             </form>
         </div>
     )
