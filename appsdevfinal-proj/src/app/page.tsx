500254a237285772e6c0e90c220d11f938153697
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2023-12-14 07:18:01 +0800

    applied new daisyUI design to the prodcut grid to look cooler

--- appsdevfinal-proj/src/app/page.tsx
+++ appsdevfinal-proj/src/app/page.tsx
@@ -1,14 +1,26 @@
 import ProductGrid from '@/components/productGrid'
 import { prisma } from '@/lib/db/prisma'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default async function Home() {
   const product = await prisma.product.findMany({
-    orderBy: {id: "desc"}
+    orderBy: { id: "desc" }
   })
   return (
-   <div>
-      <ProductGrid product={product[0]}/>
-   </div>
+    <div>
+      <div className="hero-content flex-col lg:flex-row">
+        <Image src={product[0].imageUrl}
+          alt={product[0].name}
+          width={400}
+          height={800}
+          className="max-w-sm rounded-lg shadow-2xl" />
+        <div>
+          <h1 className="text-5xl font-bold">{product[0].name}</h1>
+          <p className="py-6">{product[0].description}</p>
+          <Link href={"/products/" + product[0].id} className="btn btn-accent">Check Product</Link>
+        </div>
+      </div>
+    </div>
   )
 }
