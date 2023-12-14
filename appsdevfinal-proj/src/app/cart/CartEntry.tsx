856c025fd5ff99e32f2fceeb6ab901f916e0bbf1
JOAN KIRSTIEN BORJA <joankirstien.borja.21@usjr.edu.ph>
2023-12-15 02:28:13 +0800

    Improved cart display: linked product names to detail pages, rendered prices, added user-friendly quantity selection (1-99), and dynamic total for accurate pricing adjustments.

--- appsdevfinal-proj/src/app/cart/CartEntry.tsx
+++ appsdevfinal-proj/src/app/cart/CartEntry.tsx
@@ -1,13 +1,26 @@
 "use client";
 
 import { CartItemWithProduct } from "@/lib/db/cart";
+import { formatPrice } from "@/lib/db/format";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CartEntryProps {
     cartItem: CartItemWithProduct,
 }
 
 export default function CartEntry({cartItem : {product, quantity} }: CartEntryProps){
+
+    const quantityOptions: JSX.Element[] = [];
+    {/*Create a list of option text*/}
+    for(let i = 1; i <= 99; i++){
+        quantityOptions.push(
+            <option value={i} key={i}> 
+                {i}
+            </option>
+        );
+    }
+
     return (
         <div>
             <div className="flex flex-wrap items-center gap-3">
@@ -18,6 +31,23 @@ export default function CartEntry({cartItem : {product, quantity} }: CartEntryPr
                 height={200}
                 className="rounded-lg"
                 />
+                <div> 
+                    <Link href={"/product/" + product.id} className="font-bold"> {/*Link Product Name to Product detail page?*/}
+                        {product.name}
+                    </Link>
+                    <div>Price: {formatPrice(product.price)}</div>
+                    <div className="my-1 flex items-center gap-2">
+                        Quantity:
+                        <select
+                        className="select select-bordered w-full max-v-[80px]"
+                        >
+                           {quantityOptions} 
+                        </select>
+                    </div>
+                    <div className="flex items-center gap-3">
+                    Total: {formatPrice(product.price * quantity)}
+                    </div>
+                </div>
             </div>
             <div className="divider"/>
         </div>
