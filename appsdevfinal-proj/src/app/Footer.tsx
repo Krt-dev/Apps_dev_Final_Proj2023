4b65ea487f29ef72bc41816e864a1286e0301489
Kurt Sanchez <kurt.sanchez.20@usjr.edu.ph>
2023-12-14 21:02:49 +0800

    End of timestamp 3:26:25

---
+++ appsdevfinal-proj/src/app/Footer.tsx
@@ -0,0 +1,26 @@
+export default function Footer() {
+    return (
+      <footer className="bg-neutral p-10 text-neutral-content">
+        <div className="footer m-auto max-w-7xl">
+          <div>
+            <span className="footer-title">Kurt C. Sanchez</span>
+            <a className="link-hover link">BSCS</a>
+            <a className="link-hover link">3rd Year</a>
+            <a className="link-hover link">Likes turtles</a>
+          </div>
+          <div>
+            <span className="footer-title">Rhunnan Dwight Liao</span>
+            <a className="link-hover link">BSCS</a>
+            <a className="link-hover link">3rd Year</a>
+            <a className="link-hover link">Likes bikes</a>
+          </div>
+          <div>
+            <span className="footer-title">Joan Borja</span>
+            <a className="link-hover link">BSCS</a>
+            <a className="link-hover link">3rd Year</a>
+            <a className="link-hover link">Likes anime</a>
+          </div>
+        </div>
+      </footer>
+    );
+  }
